6ddddfb6ce23e00f9c925adce939f20bf1df7b74
Skaering <paul1995jean@gmail.com>
2017-11-23 10:56:48 +0100

    main

--- src/fr/dogstellar/Main.java
+++ src/fr/dogstellar/Main.java
@@ -2,6 +2,8 @@ package fr.dogstellar;
 
 //import fr.dogstellar.core.Player;
 import fr.dogstellar.core.*;
+import fr.dogstellar.view.Inventory;
+import fr.dogstellar.view.InventoryPanel;
 
 /**
  * @author GP3
@@ -14,10 +16,13 @@ public class Main {
 	 */
 	
 	public static void main(String[] args) {
+	Player player1 = new Player("Paul");
+	
+	
+	 Weapon stuff1= new Weapon("Excalibur","l'epe de legende",3); 
+	 Weapon stuff2= new Weapon("Sword of shadow","l'epe de tenebre",3); 
+	 player1.addStuff(stuff1,1);
+	 player1.addStuff(stuff2,1);
+	 Inventory aInventory = new Inventory(player1);
 
-		System.out.println("coucou");
-		Armor armor = new Armor ("Tata", "Tott", 4);
-
-	}
-
-}
+	}}
